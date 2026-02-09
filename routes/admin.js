ee0902b25432ff49d8a8888c6e11b4630f10914c
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-02-09 19:16:20 +0530

    added adminRouter

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const adminRouter = Router();
+
+adminRouter.post("/signin", function(req, res){
+
+})
+
+adminRouter.post("/login", function(req, res){
+
+})
+
+adminRouter.post("/course", function(req, res){
+
+})
+
+adminRouter.put("/course", function(rq, res){
+
+})
+
+adminRouter.get("/course/bulk", function(req, res){
+    
+})
+
+module.exports = {
+    adminRouter
+}
