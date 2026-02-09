c4efe2f2959c14c6e45e58b8ff9ade0bf47ea53c
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-02-09 18:24:30 +0530

    Routing structured in different files

---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const { Router } = require("express");
+
+const userRouter = Router();
+
+userRouter.post("/signup", function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const requireBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        name: z.string().min(3).max(100),
+        password: z.string().min(3).max(100)
+    })
+
+    const parsedDataWithSuccess = requireBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess)
+    {
+        res.json({
+            msg: "Incorrect format",
+            error: parsedDataWithSuccess.error
+        })
+    }
+})
+
+userRouter.post("/login", function(req, res){
+    const email = req.body.email;
+    const password = req.body.password
+
+
+
+
+})
+
+userRouter.get("/purchases", function(rq, res){
+
+})
+
+module.exports = {
+    userRouter: userRouter
+}
