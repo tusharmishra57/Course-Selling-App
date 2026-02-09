a5f432fb9067ae2f81ea988ab5d76aa8b965b727
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-02-09 20:47:22 +0530

    defined the schema

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+
 const{z} = require("zod");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course")
+const { adminRouter } = require("./routes/admin")
 const app = express();
 
 
 app.use("/user", userRouter)
 app.use("/course", courseRouter)
+app.use("/admin", adminRouter)
 
 //Login, Signup, purchase a course, sees all courses, sees the purcahsed courses
 
@@ -14,6 +19,8 @@ app.use("/course", courseRouter)
 
 
 
-
-
-app.listen(3000);
+async function main()
+{
+    await mongoose.connect("")
+    app.listen(3000);
+}
