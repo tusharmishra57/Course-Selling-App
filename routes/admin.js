344eef647f3933c030c70bf883a28f7e43904bf7
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-02-10 18:27:40 +0530

    logic for Admin login and signup

--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,93 @@ const adminRouter = Router();
 
 const { adminModel } = require("../db")
 
-adminRouter.post("/signin", function(req, res){
+const { z } = require("zod");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+const { parse } = require("dotenv");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+adminRouter.post("/signin", async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+
+    const requireBody = z.object({
+        email: z.string().min(3).max(100).email(),
+        firstName: z.string().min(3).max(100),
+        lastName: z.string().min(3).max(100),
+        password: z.string().min(3).max(100)
+    })
+
+    const parsedDataWithSuccess = requireBody.safeParse(req.body);
+
+    if(!parsedDataWithSuccess)
+    {
+        res.json({
+            msg: "Incorrect Format",
+            error: parsedDataWithSuccess.error
+        })
+    }
+
+    try
+    {
+        const hashedPassword = await bcrypt.hash(password, 5);
+
+        await adminModel.create({
+            email: email,
+            password: hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        });
+
+        res.json({
+            msg: "You are signed in"
+        })
+    }
+    catch(e)
+    {
+        if(e.code === 11000)
+        {
+            res.json({
+                msg: "User already exist"
+            })
+        }
+        else
+        {
+            res.status(500).json({
+                msg: "Internal Error"
+            })
+        }
+    }
 
 })
 
-adminRouter.post("/login", function(req, res){
+adminRouter.post("/login", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password
+
+    const user = await adminModel.findOne({
+        email: email
+    })
+
+    if(!user)
+    {
+        res.json({
+            msg: "This email doesn't exist in the database"
+        })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(passwordMatch)
+    {
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_SECRET)
+    }
+    else{
+        res.status(403).json({
+            msg: "Incorrect Password"
+        })
+    }
 
 })
 
