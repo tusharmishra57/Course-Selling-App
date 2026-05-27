ff29f682724a66344841a8ece21e4fe8b932b13d
tusharmishra57 <tusharmishra.developer@gmail.com>
2026-05-28 01:22:43 +0530

    added basic mongoDB Schema

---
+++ db.js
@@ -0,0 +1,47 @@
+// const mongoose = require("mongoose");
+// const Schema = mogoose.schema;
+
+const { Schema } = require("mongoose");  //either way is fine.
+mongoose.connect("");
+const objectId = Schema.ObjectId;
+
+const userSchema = new Schema({
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String
+
+});
+
+const adminSchema = new Schema({
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: objectId
+});
+
+const purchaseSchema = new Schema({
+    userId: objectId,
+    courseId: objectId
+});
+
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports({
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+})
